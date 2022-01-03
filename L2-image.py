2e0b47366d3dfdd6c35b1219b92b3b511a91984d
Santy <sc.santy.sayantan@gmail.com>
2022-01-03 07:22:19 +0000

    Accuracy

--- L2-image.py
+++ L2-image.py
@@ -1,22 +1,28 @@
 import tensorflow as tf
 from tensorflow import keras
 
+tf.__version__
+
 (train_x, train_y), (test_x, test_y) = keras.datasets.fashion_mnist.load_data()
+train_x = train_x/255.0
+test_x = test_x/255.0
 
 
 class myCallback(keras.callbacks.Callback):
-    def on_batch_end(self, batch, logs=None):
-        if (logs.get('loss') < 0.3):
-            print('Loss is effectively less')
-            self.model.stop_training = True
+	def on_epoch_end(self, epoch, logs=None):
+		if (logs.get('loss') < 0.3):
+			self.model.stop_training = True
+			print('Loss is effectively less')
 
 model = keras.models.Sequential(
-    [
-        keras.layers.Flatten(input_shape=(28, 28)),
-        keras.layers.Dense(128, activation=tf.nn.relu),
-        keras.layers.Dense(10, activation=tf.nn.softmax)
-    ]
+	[
+		keras.layers.Flatten(input_shape=(28, 28)),
+		keras.layers.Dense(1024, activation=tf.nn.relu),
+		keras.layers.Dense(10, activation=tf.nn.softmax)
+	]
 )
-model.compile(optimizer=tf.train.AdamOptimizer(), loss='sparse_categorical_crossentropy')
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(train_x, train_y, epochs=10, callbacks=[myCallback()])
-print("Accuracy: ", model.evaluate(test_x, test_y))
+scores = model.evaluate(test_x, test_y, return_dict=True)
+scores
+print("Test Accuracy: ", round(scores['accuracy']*100, 2), '%')
